33ba8e1de7effc91c7e7ba5a4a226cf57b3b8a11
LedLincoln <ldietrich@yahoo.com>
2022-01-28 10:53:10 -0600

    Added orange

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const app = Vue.createApp({
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
             inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
+            details: ['50% cotton', '30% wool', '20% polyester', '10% hog bristle'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2236, color: 'orange', image: '.assets/images/socks_orange.jpg'}
             ]
         }
     },
