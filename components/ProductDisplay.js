51bd5d92acc0bbaad622a02ce77b60136518dcef
LedLincoln <ldietrich@yahoo.com>
2022-02-07 15:04:33 -0600

    Added other images

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -47,12 +47,14 @@ app.component('product-display', {
   data() {
     return {
         product: 'Socks',
-        brand: 'Vue Mastery',
+        brand: 'Lincoln Looms',
         selectedVariant: 0,
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 10 },
+          { id: 2236, color: 'orange', image: './assets/images/socks_orange.jpg', quantity: 10 },
+          { id: 2237, color: 'red', image: './assets/images/socks_red.jpg', quantity: 10 },
         ],
         reviews: []
     }
